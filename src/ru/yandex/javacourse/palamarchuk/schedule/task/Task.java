4412df2620b42e8508ea6b07f192a9b773bc6e21
Palam99 <palam99andr@gmail.com>
2025-02-06 19:46:18 +0300

    тз 8.2

--- src/ru/yandex/javacourse/palamarchuk/schedule/task/Task.java
+++ src/ru/yandex/javacourse/palamarchuk/schedule/task/Task.java
@@ -2,7 +2,6 @@ package ru.yandex.javacourse.palamarchuk.schedule.task;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Locale;
 import java.util.Objects;
 
 public class Task {
--- test/ru/yandex/javacourse/palamarchuk/schedule/task/EpicTest.java
+++ test/ru/yandex/javacourse/palamarchuk/schedule/task/EpicTest.java
@@ -1,7 +1,6 @@
 package ru.yandex.javacourse.palamarchuk.schedule.task;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
